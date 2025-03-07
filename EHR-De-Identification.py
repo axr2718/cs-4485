2f2f3301753a61a7c415ff146ec1ec20325b339b
Arrio Gonsalves <72232534+ADGArrio@users.noreply.github.com>
2025-03-06 22:19:42 -0600

    Commit 1 - 2025-04-06
    1. ehr JMS.txt: Code testing file (.txt PHI file)
    2. EHR-De-identification.py: Basic PHI de-identification code
    3. deidentified_ehrJMS.txt: De-identified PHI file.

---
+++ EHR-De-Identification.py
@@ -0,0 +1,42 @@
+# %%
+import re
+
+# %%
+def deidentify_PHI(text):
+    # For Full Name
+    text = re.sub(r'Patient:\s*([A-Z][a-z]+ [A-Z][a-z]+)', r'Patient: *name*', text)
+    
+    # For Provider Name
+    text = re.sub(r'Provider:\s*Dr\.\s*([A-Z][a-z]+ [A-Z][a-z]+),\s*MD', r'Provider: Dr. *name*, MD', text)
+    
+    # For Salutations/Titles
+    text = re.sub(r'\b(Mr\.|Mrs\.|Ms\.|Dr\.)\s+[A-Z][a-z]+\b', '*name*', text)
+    
+    # For Address
+    text = re.sub(r'Address:\s*([\d\w\s,\.]+\d{5})', r'Address: *address*', text)
+    
+    # For DOB
+    text = re.sub(r'Date of Birth:\s*(\d{2}/\d{2}/\d{4})', r'Date of Birth: *dob*', text)
+    
+    # For SSN
+    text = re.sub(r'Medical Record Number:\s*(\d+)', r'Medical Record Number: *ssn*', text)
+    
+    # For Phone Number
+    text = re.sub(r'Phone:\s*(\d{3}-\d{3}-\d{4})', r'Phone: *phone*', text)
+    
+    # For Email
+    text = re.sub(r'email:\s*([\w\.-]+@[\w\.-]+\.\w+)', r'email: *email*', text)
+    
+    return text
+
+
+# %%
+with open('ehr JMS.txt', 'r') as file:
+    text = file.read()
+
+with open('deidentified_ehrJMS.txt', 'w') as file:
+    file.write(deidentify_PHI(text))
+
+print("De-identification complete. Output saved to 'deidentified_ehrJMS.txt'.")
+
+# %%
